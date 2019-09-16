6cd45993736fa468f610181358d1abd509275981
Kurt Kaiser <kurtbkaiser@gmail.com>
2019-09-15 20:45:15 -0400

    Add files via upload

---
+++ MainGame.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainGame : MonoBehaviour
+{
+    [SerializeField] Text textElement;
+    [SerializeField] State startState;
+    State state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startState;
+        textElement.text = state.GetStateText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameMain();
+    }
+
+    // Used to respond to user input
+    private void GameMain()
+    {
+        State[] statesArray = state.GetOtherStates();
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            state = statesArray[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            state = statesArray[1];
+        }
+        textElement.text = state.GetStateText();
+    }
+
+}
---
+++ State.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName =  "State")]
+public class State : ScriptableObject
+{
+    [SerializeField] string gameText;
+    [SerializeField] State[] otherStates;
+
+    // return the game text for that state
+    public string GetStateText()
+    {
+        return gameText;
+    }
+
+    // Update is called once per frame
+    public State[] GetOtherStates()
+    {
+        return otherStates;
+    }
+}
